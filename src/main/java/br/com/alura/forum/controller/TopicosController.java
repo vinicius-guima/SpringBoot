d6259ec2b1236f4cec31fdeeca57cc1290b7e1de
vini-guima <vinicus_guima.social@hotmail.com>
2020-03-21 18:54:55 -0300

    Api Rest Java Spring

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,66 @@
+package br.com.alura.forum.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+
+//@Controller
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+
+	@Autowired
+	private TopicoRepository topicoRepository; // Dependecy Injection 
+
+	@Autowired
+	private CursoRepository cursoRepository; // Dependecy Injection
+
+	
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso) {
+		// System.out.println(nomeCurso); parametro chega aqui
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}
+	
+	//boa pratica constrir uma URI que informe aonde está o obj criado com UriComponentBuilder
+
+	@PostMapping
+	private ResponseEntity<TopicoDto> cadastar(@Valid @RequestBody TopicoForm form, UriComponentsBuilder builder) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		URI uri = builder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	@GetMapping(value = "/{id}")
+	public DetalhesDoTopicoDto detalhar(@PathVariable("id") Long id) {
+		Topico topico = topicoRepository.getOne(id);
+		return new DetalhesDoTopicoDto(topico);
+		
+	}
+}
---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,22 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long>{
+	
+	//se houver um atributo de nome igual usa se "_" para dizer que é para filtrar pelo relacinamento
+	//usa se a regra nome da entidade com o atributo para filtrar pela relaçao
+	List<Topico> findByCursoNome(String nomeCurso);
+	
+	//para casos que o repository nao tem metodos necessarios
+	//monta a query usando JPQL
+	//@Para informa ao Spring que o atr que chega pelo metodo é o atr a ser usado na query
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> carregaCursoPeloNome(@Param("nomeCurso")String nomeCurso);
+}

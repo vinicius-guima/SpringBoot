2593f667657dc5f870579393aa1f9f3b9588ba59
vini-guima <vinicus_guima.social@hotmail.com>
2020-03-22 18:06:09 -0300

    implementes authorization

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	//conf de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	}
+	
+	//conf de Autorização ce url´s
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
+	}
+
+	//conf de recursos estaticos (css js imgs)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+	
+
+}
